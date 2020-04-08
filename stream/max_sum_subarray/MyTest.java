f3289698c3effdac5ef2b846a7b0f72fbd86a402
Mengxi-Zhang <zmx94824@gmail.com>
2020-04-08 02:09:36 -0400

    Add devide and conquer solution to max sum subarray

---
+++ stream/max_sum_subarray/MyTest.java
@@ -0,0 +1,32 @@
+package stream.max_sum_subarray;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyTest {
+
+    private Solution[] methods;
+
+    @Before
+    public void init() {
+        methods = new Solution[] {
+            new DivideConquer()
+        };
+    }
+
+    @Test
+    public void simpleTestCase() {
+
+        int[] input = new int[] {1, 23, -4, 5, 6};
+        int expected = 31;
+
+        for (Solution method : methods) {
+            int output = method.maxSubArray(input);
+
+            assertEquals(expected, output);
+        }
+
+    }
+}
