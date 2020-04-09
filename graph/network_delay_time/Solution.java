f7c20956802e1bb6c059eeb13cf367e5d84dc269
Mengxi-Zhang <zmx94824@gmail.com>
2020-04-09 10:42:03 -0400

    Add DFS to network-delay-time

---
+++ graph/network_delay_time/Solution.java
@@ -0,0 +1,8 @@
+package graph.network_delay_time;
+
+public interface Solution {
+    // times: [srcNode][destNode] = time needed
+    // N: number of nodes
+    // K: source of the signal
+    public int networkDelayTime(int[][] times, int N, int K);
+}
