1822cc7fe90bf952f8a96ac41ea08f50fe48f745
Mengxi-Zhang <zmx94824@gmail.com>
2020-04-10 21:16:16 -0400

    Brute force for 2d-array-min-path-sum

---
+++ two_d_array/min_path_sum/MyTest.java
@@ -0,0 +1,36 @@
+package two_d_array.min_path_sum;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyTest {
+
+    private Solution[] methods;
+
+    @Before
+    public void init() {
+        methods = new Solution[] {
+            new BruteForce()
+        };
+    }
+
+    @Test
+    public void linerTestCase() {
+
+        int[][] input = new int[][] {
+            {1, 1, 1},
+            {2, 2, 2},
+            {3, 3, 3}
+        };
+        int expectedMinPathSum = 8;
+
+        for (Solution method : methods) {
+            int output = method.minPathSum(input);
+        
+            assertEquals(expectedMinPathSum, output);
+        }
+
+    }
+}
