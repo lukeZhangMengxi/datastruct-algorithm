7b644d53bb4e39d813fa18c2c3e6eae3b0bab2ad
Mengxi Zhang <zmx94824@gmail.com>
2020-07-01 15:37:47 -0700

    Given sum find min subarray sum to the value

---
+++ patterns/sliding_window/min_len_sum/MyTest.java
@@ -0,0 +1,33 @@
+package patterns.sliding_window.min_len_sum;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyTest {
+    private Solution[] methods;
+
+    @Before
+    public void init() {
+        methods = new Solution[] {
+            new BruteForce(),
+            new ReuseSubarraySum()
+        };
+    }
+
+    @Test
+    public void simpleTestCase() throws Exception {
+
+        int[] arr = new int[] {2, 1, 5, 2, 3, 2};
+        int[] s = new int[] {8, 4};
+        int[] expected = new int[] {3, 1};
+
+        for (Solution method : methods) {
+            for (int i=0; i<s.length; i++) {
+                assertEquals(expected[i], method.findMinSubArray(s[i], arr));
+            }
+        }
+
+    }
+}
