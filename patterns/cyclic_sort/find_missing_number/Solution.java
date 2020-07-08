90bfcb954954bc385fbd56854ff3bc4008ab657e
Mengxi Zhang <zmx94824@gmail.com>
2020-07-08 15:12:09 -0700

    cyclic sort find missing number

---
+++ patterns/cyclic_sort/find_missing_number/Solution.java
@@ -0,0 +1,12 @@
+package patterns.cyclic_sort.find_missing_number;
+
+
+public abstract class Solution {
+
+    abstract public int findMissingNumber(int[] nums) throws Exception;
+
+    protected void isValid(int[] nums) throws Exception {
+        
+    }
+
+}
