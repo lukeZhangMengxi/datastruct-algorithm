55d0321a308d4b4b83a456f6119f46ca698f83f4
Mengxi Zhang <zmx94824@gmail.com>
2020-07-12 21:57:02 -0700

    brute force for medium of number stream

---
+++ patterns/two_heaps/meidum_of_number_stream/Solution.java
@@ -0,0 +1,13 @@
+package patterns.two_heaps.meidum_of_number_stream;
+
+
+public abstract class Solution {
+
+    abstract public void insertNum(int num);
+
+    abstract public double findMedian();
+
+    protected void isValid(int num) throws Exception {
+        
+    }
+}
