59b176891a82e0c83e257df4cfa9b7483c75da70
Mengxi Zhang <zmx94824@gmail.com>
2020-07-16 03:05:12 -0700

    prepare for test one

---
+++ test/one/Solution.java
@@ -0,0 +1,11 @@
+package test.one;
+
+public interface Solution {
+
+    public void init(int N, int[] nums);
+
+    public int queryMaxGrade(int startStudentId, int endStudentId);
+
+    public void updateGrade(int studentId, int grade);
+
+}
