75d2c61e8611754df5d8a5732f65838453b35999
Mengxi Zhang <zmx94824@gmail.com>
2020-07-16 18:40:02 -0700

    mock four

---
+++ test/four/Solution.java
@@ -0,0 +1,11 @@
+package test.four;
+
+import java.util.List;
+
+public interface Solution {
+    
+    public void addString(String s);
+
+    public List<String> getResult();
+
+}
---
+++ test/four/SolutionImpl.java
@@ -0,0 +1,33 @@
+package test.four;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionImpl implements Solution {
+
+    private List<String> rst;
+
+    SolutionImpl() {
+        this.rst = new ArrayList<>();
+    }
+
+    @Override
+    public void addString(String s) {
+        if (s.length() == 0) return;
+
+        while (s.length() % 8 != 0) s = s+"0";
+        
+        int idx = 0;
+        while (idx <= s.length()-8) {
+            rst.add(s.substring(idx, idx+8));
+            idx+=8;
+        }
+
+    }
+
+    @Override
+    public List<String> getResult() {
+        return rst;
+    }
+    
+}
