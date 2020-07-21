f79e78a687cebd3bb6693ccc2d9458334ad1bd38
Mengxi <zmx94824@gmail.com>
2020-07-21 15:58:58 -0700

    Figure out memoization runtime issue (#3)
    * capture the wierd behaviour
    
    * Split UnitTestStopWatch to a extendable class
    
    * Refactor knapsack
    
    * Add knapsack tabulation and fix memoization error
    
    * Fixed by: DO NOT pass return value as a parameter

--- dp/knapsack/MyTest.java
+++ dp/knapsack/MyTest.java
@@ -5,15 +5,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MyTest {
+import lib.UnitTestStopWatch;
+
+public class MyTest extends UnitTestStopWatch {
 
     private Solution[] solutions;
 
     @Before
     public void init() {
         solutions = new Solution[] {
-            new Recursion(),
+            // new Recursion()
             new Memoization()
+            // new Tabulation()
         };
     }
     
@@ -26,6 +29,22 @@ public class MyTest {
                     new int[] {1, 2, 4, 6},
                     new int[] {4, 2, 4, 7},
                     7
+                )
+            );
+        }
+    }
+
+    @Test
+    public void largeInput() {
+        int[] weights = new int[] {28,41,21,48,85,80,39,46,21,97,67,79,26,14,95,12,89,18,52,52,12,88,61,69,48,48,62,74,11,5,3,38,12,53,80,90,16,65,44,25,41,53,58,38,4,72,55,44,30,42,5,9,21,24,85,59,44,59,8,29,41,44,4,26,63,99,18,70,38,47,23,90,19,24,34,31,57,46,36,96,54,75,93,21,83,36,42,2,43,85,21,57,23,96,64,49,91,96,10};
+        int[] profits = new int[] {60,20,16,42,83,68,85,69,88,48,29,59,18,53,7,15,18,53,93,57,93,63,49,68,70,19,62,81,60,96,92,89,45,28,61,83,10,52,52,34,16,18,64,20,14,86,65,27,33,49,21,3,78,46,11,81,53,70,92,23,50,7,51,48,58,90,65,21,60,89,91,37,33,72,26,3,67,92,46,30,0,30,0,78,11,71,60,4,79,34,8,82,39,64,71,46,17,58,7};
+        for (Solution m: solutions) {
+            assertEquals(
+                899,
+                m.maxProfit(
+                    weights,
+                    profits,
+                    150     // Increase this value to allow DFS goes deeper, and the recursion would be much much slower
                 )
             );
         }
--- dp/knapsack/Solution.java
+++ dp/knapsack/Solution.java
@@ -10,16 +10,12 @@ interface Solution {
 
 class Recursion implements Solution {
 
-    private boolean selected[];
-
     private int maxP(int[] weights, int[] prices, int capacity, int cumulativeProfit, int i) {
         if (i == weights.length || capacity == 0) return cumulativeProfit;
 
         int selectThisProfit = 0, notSelectThisProfit = 0;
-        if (!selected[i] && weights[i] <= capacity) {
-            selected[i] = true;
+        if (weights[i] <= capacity) {
             selectThisProfit = maxP(weights, prices, capacity-weights[i], cumulativeProfit+prices[i], i+1);
-            selected[i] = false;
         }
         notSelectThisProfit = maxP(weights, prices, capacity, cumulativeProfit, i+1);
 
@@ -28,7 +24,6 @@ class Recursion implements Solution {
 
     @Override
     public int maxProfit(int[] weights, int[] prices, int capacity) {
-        selected = new boolean[weights.length];
         return maxP(weights, prices, capacity, 0, 0);
     }
 
@@ -37,19 +32,16 @@ class Recursion implements Solution {
 class Memoization implements Solution {
 
     private int[][] profitBySelectAndCapacity;
-    private boolean selected[];
 
-    private int maxP(int[] weights, int[] prices, int capacity, int cumulativeProfit, int i) {
-        if (i == weights.length || capacity == 0) return cumulativeProfit;
+    private int maxP(int[] weights, int[] prices, int capacity, int i) {
+        if (i == weights.length || capacity == 0) return 0;
         if (profitBySelectAndCapacity[i][capacity] != -1) return profitBySelectAndCapacity[i][capacity];
 
         int selectThisProfit = 0, notSelectThisProfit = 0;
-        if (!selected[i] && weights[i] <= capacity) {
-            selected[i] = true;
-            selectThisProfit = maxP(weights, prices, capacity-weights[i], cumulativeProfit+prices[i], i+1);
-            selected[i] = false;
+        if (weights[i] <= capacity) {
+            selectThisProfit = maxP(weights, prices, capacity-weights[i], i+1) + prices[i];
         }
-        notSelectThisProfit = maxP(weights, prices, capacity, cumulativeProfit, i+1);
+        notSelectThisProfit = maxP(weights, prices, capacity, i+1);
 
         profitBySelectAndCapacity[i][capacity] = Math.max(selectThisProfit, notSelectThisProfit);
         return profitBySelectAndCapacity[i][capacity];
@@ -57,12 +49,36 @@ class Memoization implements Solution {
 
     @Override
     public int maxProfit(int[] weights, int[] prices, int capacity) {
-        selected = new boolean[weights.length];
         profitBySelectAndCapacity = new int[weights.length][capacity+1];
         for (int[] r: profitBySelectAndCapacity)
             Arrays.fill(r, -1);
-        return maxP(weights, prices, capacity, 0, 0);
+        return maxP(weights, prices, capacity, 0);
     }
 
 }
 
+class Tabulation implements Solution {
+
+    @Override
+    public int maxProfit(int[] weights, int[] prices, int capacity) {
+        int[][] profitByIndexAndCapacity = new int[weights.length][capacity+1];
+        
+        // When capacity is zero, profit is also zero
+        for (int i=0; i<weights.length; i++) profitByIndexAndCapacity[i][0] = 0;
+
+        // When index is 0, the max profit we could get is weights[i]
+        for (int i=0; i<capacity+1; i++)
+            if (i >= weights[0]) profitByIndexAndCapacity[0][i] = prices[0];
+        
+        for (int i=1; i<weights.length; i++) {
+            for (int cap=1; cap<capacity+1; cap++) {
+                int profit1 = profitByIndexAndCapacity[i-1][cap];
+                int profit2 = (cap - weights[i] >= 0) ? prices[i] + profitByIndexAndCapacity[i-1][cap - weights[i]] : 0;
+                profitByIndexAndCapacity[i][cap] = Math.max(profit1, profit2);
+            }
+        }
+
+        return profitByIndexAndCapacity[weights.length-1][capacity];
+    }
+
+}
