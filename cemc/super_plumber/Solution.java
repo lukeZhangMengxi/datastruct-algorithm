ede2708f606ee2fe5cda6ac73813c518af761e00
Mengxi Zhang <zmx94824@gmail.com>
2020-08-03 12:42:56 -0700

    Further reduced the context

--- cemc/super_plumber/Solution.java
+++ cemc/super_plumber/Solution.java
@@ -62,24 +62,29 @@ class DFSBacktrackReducedContext implements Solution {
 
 class Memoization implements Solution {
 
-    private String encodeKey(int r, int c, int lastR, int lastC) {
-        return r + "#" + c + "#" + lastR + "#" + lastC + "#";
+    private final int RIGHT = 0;
+    private final int UP = 1;
+    private final int DOWN = 2;
+
+
+    private String encodeKey(int r, int c, int last) {
+        return r + "#" + c + "#" + last;
     }
 
-    private int dfs(char[][] grid, int r, int c, int lastR, int lastC, Map<String, Integer> dp) {
+    private int dfs(char[][] grid, int r, int c, int last, Map<String, Integer> dp) {
         if (r<0 || c <0 || r>=grid.length || c>=grid[0].length) return -1;
         if (grid[r][c] == '*') return Integer.MIN_VALUE;
 
-        String dpKey = encodeKey(r, c, lastR, lastC);
+        String dpKey = encodeKey(r, c, last);
         if (dp.containsKey(dpKey)) return dp.get(dpKey);
 
         int curScore = (grid[r][c] == '.') ? 0 : Character.getNumericValue(grid[r][c]);
         if (r == grid.length-1 && c == grid[0].length-1) return curScore;
         
         int maxRestScore = Integer.MIN_VALUE;
-        if (r+1 != lastR || c != lastC) maxRestScore = dfs(grid, r+1, c, r, c, dp);
-        if (r-1 != lastR || c != lastC) maxRestScore = Math.max(maxRestScore, dfs(grid, r-1, c, r, c, dp));
-        if (r != lastR || c+1 != lastC) maxRestScore = Math.max(maxRestScore, dfs(grid, r, c+1, r, c, dp));
+        if (last != DOWN) maxRestScore = dfs(grid, r+1, c, UP, dp);
+        if (last != UP) maxRestScore = Math.max(maxRestScore, dfs(grid, r-1, c, DOWN, dp));
+        maxRestScore = Math.max(maxRestScore, dfs(grid, r, c+1, RIGHT, dp));
 
         dp.put(dpKey, curScore + maxRestScore);
         return curScore + maxRestScore;
@@ -88,7 +93,7 @@ class Memoization implements Solution {
     @Override
     public int getMaxScore(char[][] grid) {
         
-        return dfs(grid, grid.length-1, 0, -1, -1, new HashMap<>());
+        return dfs(grid, grid.length-1, 0, RIGHT, new HashMap<>());
     }
 
 }
