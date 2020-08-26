94d40485f98e8228efb3d77cbba3d55eba2e6436
Mengxi Zhang <zmx94824@gmail.com>
2020-08-25 17:42:18 -0700

    Update TestUtils.java

--- lib/TestUtils.java
+++ lib/TestUtils.java
@@ -7,11 +7,19 @@ import java.util.List;
 
 public class TestUtils {
 
-    public static void assertListEquals(List<String> a, List<String> b) {
+    public static void assertStringListEquals(List<String> a, List<String> b) {
         assertEquals(a.size(), b.size());
         
         Collections.sort(a);
         Collections.sort(b);
+        for (int i=0; i<a.size(); i++) {
+            assertEquals(a.get(i), b.get(i));
+        }
+    }
+
+    public static <T> void assertListEquals(List<T> a, List<T> b) {
+        assertEquals(a.size(), b.size());
+
         for (int i=0; i<a.size(); i++) {
             assertEquals(a.get(i), b.get(i));
         }
