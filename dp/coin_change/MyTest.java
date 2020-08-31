fdda274e5b095a34178092d2d20dfb4dfe98d3ef
Mengxi Zhang <zmx94824@gmail.com>
2020-08-30 21:27:53 -0700

    Minimum state presentation, over 300 times faster

--- dp/coin_change/MyTest.java
+++ dp/coin_change/MyTest.java
@@ -9,7 +9,7 @@ import lib.MyUnitTests;
 public class MyTest extends MyUnitTests {
     @Test
     public void simple() {
-        Solution s = new BF();
+        Solution s = new BetterMEM();
 
         assertEquals(3, s.coinChange(new int[] {1,2,5}, 11));
         assertEquals(-1, s.coinChange(new int[] {2}, 3));
@@ -17,14 +17,21 @@ public class MyTest extends MyUnitTests {
 
     @Test(timeout = 100)
     public void largerInput() {
-        Solution s = new MEM();
+        Solution s = new BadMEM();
 
         assertEquals(20, s.coinChange(new int[] {1,2,5}, 100));
     }
 
-    @Test(timeout = 3000)
+    @Test(timeout = 3500)
     public void evenLargerInput() {
-        Solution s = new MEM();
+        Solution s = new BadMEM();
+
+        assertEquals(25, s.coinChange(new int[] {3, 7, 405, 436}, 8839));
+    }
+
+    @Test(timeout = 10)
+    public void evenLargerInput1() {
+        Solution s = new BetterMEM();
 
         assertEquals(25, s.coinChange(new int[] {3, 7, 405, 436}, 8839));
     }
--- dp/coin_change/Solution.java
+++ dp/coin_change/Solution.java
@@ -6,7 +6,34 @@ interface Solution {
     int coinChange(int[] coins, int amount);
 }
 
-class MEM implements Solution {
+class BetterMEM implements Solution {
+
+    int dfs(int[] sortedCoins, int amount, Integer[] dp) {
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
+        if (dp[amount] != null) return dp[amount];
+
+        int result = -1;
+        for (int i=sortedCoins.length-1; i>=0; i--) {
+            int tmp = dfs(sortedCoins, amount-sortedCoins[i], dp);
+            if (tmp != -1) {
+                result = (result==-1) ? tmp + 1 : Math.min(result, tmp + 1);
+            }
+        }
+
+        dp[amount] = result;
+        return result;
+    }
+
+    @Override
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        return dfs(coins, amount, new Integer[amount+1]);
+    }
+
+}
+
+class BadMEM implements Solution {
 
     int dfs(int[] sortedCoins, int amount, int buf, Integer[][] dp) {
         if (amount == 0) return buf;
