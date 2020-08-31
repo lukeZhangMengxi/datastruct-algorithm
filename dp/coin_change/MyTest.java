baeb1c54f08fe91fb0c8d5e2fa2b5c1c35e69717
Mengxi <zmx94824@gmail.com>
2020-08-30 21:52:50 -0700

    One more DFS to Tabulation path: Coin Change (#11)
    * Slow Memoization
    
    * Minimum state presentation for memoization, over 300 times faster
    
    * Bottom-up tabulation makes it even 2 times faster than memoization

---
+++ dp/coin_change/MyTest.java
@@ -0,0 +1,45 @@
+package dp.coin_change;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import lib.MyUnitTests;
+
+public class MyTest extends MyUnitTests {
+    @Test
+    public void simple() {
+        Solution s = new Tabulation();
+
+        assertEquals(3, s.coinChange(new int[] {1,2,5}, 11));
+        assertEquals(-1, s.coinChange(new int[] {2}, 3));
+    }
+
+    @Test(timeout = 100)
+    public void largerInput() {
+        Solution s = new BadMEM();
+
+        assertEquals(20, s.coinChange(new int[] {1,2,5}, 100));
+    }
+
+    @Test(timeout = 3500)
+    public void evenLargerInput() {
+        Solution s = new BadMEM();
+
+        assertEquals(25, s.coinChange(new int[] {3, 7, 405, 436}, 8839));
+    }
+
+    @Test(timeout = 14)
+    public void evenLargerInput2() {
+        Solution s = new BetterMEM();
+
+        assertEquals(25, s.coinChange(new int[] {3, 7, 405, 436}, 8839));
+    }
+
+    @Test(timeout = 7)
+    public void evenLargerInput3() {
+        Solution s = new Tabulation();
+
+        assertEquals(25, s.coinChange(new int[] {3, 7, 405, 436}, 8839));
+    }
+}
---
+++ dp/coin_change/Solution.java
@@ -0,0 +1,108 @@
+package dp.coin_change;
+
+import java.util.Arrays;
+
+interface Solution {
+    int coinChange(int[] coins, int amount);
+}
+
+class Tabulation implements Solution {
+
+    @Override
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+
+        int[] dp = new int[amount+1];
+        for (int c: coins) { 
+            if (c < dp.length) dp[c] = 1;
+        }
+
+        for (int i=1; i<dp.length; i++) {
+            int tmp = -1;
+            for (int c: coins) {
+                if (i-c >= 0 && dp[i-c] != -1) {
+                    tmp = (tmp==-1) ? dp[i-c]+1 : Math.min(tmp, dp[i-c]+1);
+                }
+            }
+            dp[i] = tmp;
+        }
+
+        return dp[amount];
+    }
+
+}
+
+class BetterMEM implements Solution {
+
+    int dfs(int[] sortedCoins, int amount, Integer[] dp) {
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
+        if (dp[amount] != null) return dp[amount];
+
+        int result = -1;
+        for (int i=sortedCoins.length-1; i>=0; i--) {
+            int tmp = dfs(sortedCoins, amount-sortedCoins[i], dp);
+            if (tmp != -1) {
+                result = (result==-1) ? tmp + 1 : Math.min(result, tmp + 1);
+            }
+        }
+
+        dp[amount] = result;
+        return result;
+    }
+
+    @Override
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        return dfs(coins, amount, new Integer[amount+1]);
+    }
+
+}
+
+class BadMEM implements Solution {
+
+    int dfs(int[] sortedCoins, int amount, int buf, Integer[][] dp) {
+        if (amount == 0) return buf;
+        if (amount < 0) return -1;
+        if (dp[amount][buf] != null) return dp[amount][buf];
+
+        int result = -1;
+        for (int i=sortedCoins.length-1; i>=0; i--) {
+            int tmp = dfs(sortedCoins, amount-sortedCoins[i], buf+1, dp);
+            if (tmp != -1) result = (result==-1) ? tmp: Math.min(result, tmp);
+        }
+
+        dp[amount][buf] = result;
+        return result;
+    }
+
+    @Override
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        return dfs(coins, amount, 0, new Integer[amount+1][amount/coins[0]+1]);
+    }
+
+}
+
+class BF implements Solution {
+
+    int dfs(int[] sortedCoins, int amount, int buf) {
+        if (amount == 0) return buf;
+        if (amount < 0) return -1;
+
+        int result = -1;
+        for (int i=sortedCoins.length-1; i>=0; i--) {
+            int tmp = dfs(sortedCoins, amount-sortedCoins[i], buf+1);
+            if (tmp != -1) result = (result==-1) ? tmp: Math.min(result, tmp);
+        }
+
+        return result;
+    }
+
+    @Override
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        return dfs(coins, amount, 0);
+    }
+
+}
