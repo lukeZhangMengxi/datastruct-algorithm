97c864c50da0187701a6a77a5c0e0013661b7412
Mengxi Zhang <mengxi.zhang@sap.com>
2021-03-20 21:20:06 -0700

    Play with java.util.timer

---
+++ java_lang_interface/java_util/timer/MyTest.java
@@ -0,0 +1,28 @@
+package java_lang_interface.java_util.timer;
+
+import org.junit.Test;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyTest {
+
+    class Target {
+        // Timer timer;
+        public Target() {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask(){
+                @Override
+                public void run() {
+                    System.out.println("I am running!!!");
+                }
+            }, 0, 1*1000);
+        }
+    }
+
+    @Test
+    public void simple() throws InterruptedException {
+        new Target();
+        Thread.currentThread().sleep(5000);
+    }
+}
